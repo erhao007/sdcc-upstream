acf99bb5a95f477309362dc4e51fb427a3614865
zhangerhao <251819788@qq.com>
2026-08-10 21:38:05 +0800

    regression: bug-3249 add mcs251 to DATA define (addressmod not supported)
    mcs251 doesn't support __addressmod (embedded C named address spaces).
    Add __SDCC_mcs251 alongside __SDCC_mcs51 so the test uses '#define DATA'
    (empty) instead of the addressmod path.

--- support/regression/tests/bug-3249.c
+++ support/regression/tests/bug-3249.c
@@ -9,7 +9,7 @@ volatile char tmp;
 
 void set_RAM_bank1(void) { tmp = 1; }
 
-#if defined(__SDCC_mcs51)
+#if defined(__SDCC_mcs51) || defined(__SDCC_mcs251)
 #define DATA
 #elif !defined(PORT_HOST)
 __addressmod set_RAM_bank1 DATA;
