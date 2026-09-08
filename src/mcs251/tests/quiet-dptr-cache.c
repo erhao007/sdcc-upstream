9fcb4ac7b5b83da0e6f35f235e98f01367d60029
zhangerhao <251819788@qq.com>
2026-09-08 22:01:12 +0800

    MT-6B: silence internal DPTR cache tracing

---
+++ src/mcs251/tests/quiet-dptr-cache.c
@@ -0,0 +1,23 @@
+static __xdata unsigned char info[18];
+
+void fill_info(void)
+{
+  info[0] = 2;
+  info[1] = 1;
+  info[2] = 0;
+  info[3] = 128;
+  info[4] = 0;
+  info[5] = 1;
+  info[6] = 0;
+  info[7] = 0;
+  info[8] = 48;
+  info[9] = 255;
+  info[10] = 0;
+  info[11] = 0;
+  info[12] = 208;
+  info[13] = 0;
+  info[14] = 0;
+  info[15] = 128;
+  info[16] = 0;
+  info[17] = 1;
+}
